e08b5dea43fcb76e2e1a192d5ddd1fbee4445842
AndresFerreiraR <ac.ferreira.r@gmail.com>
2022-03-28 16:01:43 -0500

    Agregar archivos de proyecto.

---
+++ 1. SOLID/1.1 Single Responsibility/SingleResponsibility/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//.WriteLine("Hello, World!");
+
+using SingleResponsibility.Clase;
+using SingleResponsibility.Modelo;
+
+var report = new CourseReport();
+report.AddEntry(new CourseReportEntry { Name = "Patrones de diseño", Students = 1000, Rating = 5 });
+report.AddEntry(new CourseReportEntry { Name = "Flutter", Students = 1900, Rating = 4.5 });
+
+Console.WriteLine(report.ToString());
+
+report.SaveToFile(@"Reports", "WorkReport.txt");
