88f17cd30a0f29bf6a9bc2396fc0b273e4bf1ee3
AndresFerreiraR <ac.ferreira.r@gmail.com>
2022-04-08 14:55:54 -0500

    Cambios de builder creacion de proyecto y primer problema

---
+++ 2. PatronesCreacionales/2.1 Builder/Builder/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Builder.Clases;
+
+Console.WriteLine("Hello, World!");
+
+
+var sandwich = new Sandwich("Pan frances", "Tomate, Lechuga, Cebolla", "Pavo", "", "Motzarella");
