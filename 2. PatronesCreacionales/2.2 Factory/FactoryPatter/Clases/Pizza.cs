1e2b54b08cf01ce1f40693e75b29912f25425276
AndresFerreiraR <ac.ferreira.r@gmail.com>
2022-06-21 16:21:48 -0500

    Cambios realacionados con Abstract factory

--- 2. PatronesCreacionales/2.2 Factory/FactoryPatter/Clases/Pizza.cs
+++ 2. PatronesCreacionales/2.2 Factory/FactoryPatter/Clases/Pizza.cs
@@ -13,13 +13,15 @@ namespace FactoryPatter.Clases
         protected string Sauce { get; set; }
         protected List<string> Toppings = new List<string>();
 
-        public void Prepare()
-        {
-            Console.WriteLine($"Preparando: {Name}");
-            Console.WriteLine($"Colocando masa: {Dough}");
-            Console.WriteLine($"Agregando Salsa: {Sauce}");
-            Console.WriteLine($"Agregando capas: {string.Join(",", Toppings)}");
-        }
+        //public void Prepare()
+        //{
+        //    Console.WriteLine($"Preparando: {Name}");
+        //    Console.WriteLine($"Colocando masa: {Dough}");
+        //    Console.WriteLine($"Agregando Salsa: {Sauce}");
+        //    Console.WriteLine($"Agregando capas: {string.Join(",", Toppings)}");
+        //}
+
+
 
         public void Bake() => Console.WriteLine("Cocinar por 20 min");
         public void Cut() => Console.WriteLine("Pizza cortada en partes iguales");
