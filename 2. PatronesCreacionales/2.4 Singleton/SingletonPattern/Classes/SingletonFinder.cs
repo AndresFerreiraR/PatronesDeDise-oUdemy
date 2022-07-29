f2395308f6ef020cda08d0a3fc374f2c6cfea4b1
AndresFerreiraR <ac.ferreira.r@gmail.com>
2022-07-29 18:54:33 -0500

    Se crea Clase para acceder y sumar la poblacion de las ciudades, recibe como parametro una coleccion de string con los nombres y retorna la sumatoria de las poblaciones. se crea proyecto de prueba unitaria

---
+++ 2. PatronesCreacionales/2.4 Singleton/SingletonPattern/Classes/SingletonFinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern.Classes
+{
+    public class SingletonFinder
+    {
+        public int GetTotalPopulation(IEnumerable<string> names)
+        {
+            int totalPopulation = 0;
+            foreach (string name in names)
+            {
+                totalPopulation += SingletonDataContainer.Instance.GetPopulation(name);
+            }
+
+            return totalPopulation;
+        }
+    }
+}
